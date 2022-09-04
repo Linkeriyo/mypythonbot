9faaf19317ab41eb712d9701f6a385618fdef394
linkeriyo <linkeriyo@gmail.com>
2022-09-04 19:36:40 +0200

    little tweak

--- commands/fakedelete/command.py
+++ commands/fakedelete/command.py
@@ -2,5 +2,5 @@ import random
 
 
 async def run(message, params):
-    rows = int(random.random()*5)+4
+    rows = int(random.random()*30)+4
     await message.reply(f'Result: {rows} rows deleted from {params[1]}')
